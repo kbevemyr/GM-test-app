e03e68ca8cfb337d746e7331d257921d4c982445
Katrin Boberg Bevemyr <katrin@bevemyr.com>
2019-04-01 16:55:57 +0200

    added withRouter

--- src/Peoplelist.js
+++ src/Peoplelist.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import { connect } from 'react-redux';
 import { getMyFiles } from './store/actions';
 
-//import { Route, Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
